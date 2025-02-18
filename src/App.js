2ec3028006be2b48104df8f14a29e396ad67e602
manoharreddyparne <parnemanoharreddy19@gmail.com>
2025-02-18 15:53:25 +0530

    added search bar and modified the structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./default_dashboard/Navbar.jsx";
-import DashboardRoutes from "./default_dashboard/DashboardRoutes.jsx";
+import Navbar from "./default_dashboard/components/Navbar";
+import DashboardRoutes from "./default_dashboard/jsx/DashboardRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./default_dashboard/global.css";  // Global CSS
+import "./default_dashboard/global.css"; // Global CSS
 
 function App() {
+  // State for search query
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <>
       {/* Global Background applied once */}
@@ -13,7 +16,7 @@ function App() {
 
       {/* Router wrapping the Navbar and Routes */}
       <Router>
-        <Navbar />
+        <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         <DashboardRoutes />
       </Router>
     </>
